4246405a5cfd37bb6cb59789f71a3cbcac557f54
danielbednarz2 <danielbednarz2@gmail.com>
2021-03-08 14:43:13 -0800

    Initial Commit w/ Strapi and React Project

---
+++ blog-server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd6948e938e0df6c0bf69450abe676d98'),
+    },
+  },
+});
---
+++ blog-server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dcd8627f-2954-4b37-81ab-cbf8f43ea08c'
+};
